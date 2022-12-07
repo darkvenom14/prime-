93add11d74f625c76bf837f669902b6a85ebce87
01patilrana <patilrana845@gmail.com>
2022-12-07 22:31:34 +0530

    prime code was fix

--- prime.js
+++ prime.js
@@ -1,9 +1,9 @@
 let num = 13 ;
 let count=0;
-for( i=1 ; i<=num ; i++ ){
-  if( num % i ==1 ){
+for( i=0 ; i<=num ; i++ ){
+  if( num % i ==0 ){
     count++
   }
 } 
-( count == 0 )? console.log("yes"):
+( count == 2 )? console.log("yes"):
   console.log("no")
